14454059cfdc531f7be73a633c6f91f09b48ed07
Porjo <porjo38@yahoo.com.au>
2014-07-10 17:05:28 +1000

    Add 'rewrite' middleware

--- init.go
+++ init.go
@@ -4,6 +4,7 @@ import (
 	"github.com/mailgun/vulcand/plugin"
 	"github.com/mailgun/vulcand/plugin/connlimit"
 	"github.com/mailgun/vulcand/plugin/ratelimit"
+	"github.com/mailgun/vulcand/plugin/rewrite"
 )
 
 func GetRegistry() (*plugin.Registry, error) {
@@ -14,6 +15,10 @@ func GetRegistry() (*plugin.Registry, error) {
 	}
 
 	if err := r.AddSpec(connlimit.GetSpec()); err != nil {
+		return nil, err
+	}
+
+	if err := r.AddSpec(rewrite.GetSpec()); err != nil {
 		return nil, err
 	}
 	return r, nil
---
+++ plugin/rewrite/rewrite_test.go
@@ -0,0 +1,70 @@
+package rewrite
+
+import (
+	"regexp"
+	"testing"
+
+	"github.com/mailgun/vulcand/Godeps/_workspace/src/github.com/codegangsta/cli"
+	. "github.com/mailgun/vulcand/Godeps/_workspace/src/launchpad.net/gocheck"
+	"github.com/mailgun/vulcand/plugin"
+)
+
+func TestRL(t *testing.T) { TestingT(t) }
+
+type RewriteSuite struct {
+}
+
+var _ = Suite(&RewriteSuite{})
+
+// One of the most important tests:
+// Make sure the Rewrite spec is compatible and will be accepted by middleware registry
+func (s *RewriteSuite) TestSpecIsOK(c *C) {
+	c.Assert(plugin.NewRegistry().AddSpec(GetSpec()), IsNil)
+}
+
+func (s *RewriteSuite) TestNewRewriteSuccess(c *C) {
+	ri, err := NewRewriteInstance("^/foo(.*)", "$1")
+	c.Assert(ri, NotNil)
+	c.Assert(err, IsNil)
+
+	out, err := ri.NewMiddleware()
+	c.Assert(out, NotNil)
+	c.Assert(err, IsNil)
+}
+
+func (s *RewriteSuite) TestNewRewriteBadParams(c *C) {
+	// Bad regex
+	_, err := NewRewriteInstance("[", "")
+	c.Assert(err, NotNil)
+}
+
+func (s *RewriteSuite) TestNewRewriteFromOther(c *C) {
+	ri, err := NewRewriteInstance("^/foo(.*)", "$1")
+	c.Assert(err, IsNil)
+
+	r := Rewrite{"^/foo(.*)", "$1"}
+
+	out, err := FromOther(r)
+	c.Assert(err, IsNil)
+	c.Assert(out, DeepEquals, ri)
+}
+
+func (s *RewriteSuite) TestNewRewriteFromCliOk(c *C) {
+	app := cli.NewApp()
+	app.Name = "test"
+	executed := false
+	app.Action = func(ctx *cli.Context) {
+		executed = true
+		out, err := FromCli(ctx)
+		c.Assert(out, NotNil)
+		c.Assert(err, IsNil)
+
+		ri := out.(*RewriteInstance)
+		re, _ := regexp.Compile("^/foo(.*)")
+		c.Assert(ri.regexp.String(), Equals, re.String())
+		c.Assert(ri.replacement, Equals, "$1")
+	}
+	app.Flags = CliFlags()
+	app.Run([]string{"test", "--regexp=^/foo(.*)", "--replacement=$1"})
+	c.Assert(executed, Equals, true)
+}
