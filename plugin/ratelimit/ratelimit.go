0c17740165a70671ce590227031a456fc93f890d
Maxim Vladimirsky <horkhe@gmail.com>
2014-11-25 17:34:20 -0800

    Make use of vulcand dynamic bucket config

--- plugin/ratelimit/ratelimit.go
+++ plugin/ratelimit/ratelimit.go
@@ -1,86 +1,154 @@
 package ratelimit
 
 import (
+	"encoding/json"
 	"fmt"
+	"time"
+
 	"github.com/mailgun/vulcand/Godeps/_workspace/src/github.com/codegangsta/cli"
+	"github.com/mailgun/vulcand/Godeps/_workspace/src/github.com/mailgun/timetools"
 	"github.com/mailgun/vulcand/Godeps/_workspace/src/github.com/mailgun/vulcan/limit"
 	"github.com/mailgun/vulcand/Godeps/_workspace/src/github.com/mailgun/vulcan/limit/tokenbucket"
 	"github.com/mailgun/vulcand/Godeps/_workspace/src/github.com/mailgun/vulcan/middleware"
+	"github.com/mailgun/vulcand/Godeps/_workspace/src/github.com/mailgun/vulcan/request"
 	"github.com/mailgun/vulcand/plugin"
-	"time"
 )
 
-const Type = "ratelimit"
-
-// Spec is an entry point of a plugin and will be called to register this middleware plugin withing vulcand
+// GetSpec is an entry point of a plugin and will be called to register this
+// middleware plugin with vulcand.
 func GetSpec() *plugin.MiddlewareSpec {
-	return &plugin.MiddlewareSpec{
-		Type:      Type,
-		FromOther: FromOther,
-		FromCli:   FromCli,
-		CliFlags:  CliFlags(),
-	}
-}
-
-// Rate controls how many requests per period of time is allowed for a location.
-// Existing implementation is based on the token bucket algorightm http://en.wikipedia.org/wiki/Token_bucket
-type RateLimit struct {
-	PeriodSeconds int    // Period in seconds, e.g. 3600 to set up hourly rates
-	Burst         int64  // Burst count, allowes some extra variance for requests exceeding the average rate
-	Variable      string // Variable defines how the limiting should be done. e.g. 'client.ip' or 'request.header.X-My-Header'
-	Requests      int    // Allowed average requests
-}
-
-// Returns vulcan library compatible middleware
-func (r *RateLimit) NewMiddleware() (middleware.Middleware, error) {
-	mapper, err := limit.VariableToMapper(r.Variable)
-	if err != nil {
-		return nil, err
-	}
-	rate := tokenbucket.Rate{Units: int64(r.Requests), Period: time.Second * time.Duration(r.PeriodSeconds)}
-	return tokenbucket.NewTokenLimiterWithOptions(mapper, rate, tokenbucket.Options{Burst: r.Burst})
-}
-
-func NewRateLimit(requests int, variable string, burst int64, periodSeconds int) (*RateLimit, error) {
-	if _, err := limit.VariableToMapper(variable); err != nil {
-		return nil, err
-	}
-	if requests <= 0 {
-		return nil, fmt.Errorf("requests should be > 0, got %d", requests)
-	}
-	if burst < 0 {
-		return nil, fmt.Errorf("burst should be >= 0, got %d", burst)
-	}
-	if periodSeconds <= 0 {
-		return nil, fmt.Errorf("period seconds should be > 0, got %d", periodSeconds)
-	}
-	return &RateLimit{
-		Requests:      requests,
-		Variable:      variable,
-		Burst:         burst,
-		PeriodSeconds: periodSeconds,
-	}, nil
-}
-
-func (rl *RateLimit) String() string {
-	return fmt.Sprintf("var=%s, reqs/%s=%d, burst=%d",
-		rl.Variable, time.Duration(rl.PeriodSeconds)*time.Second, rl.Requests, rl.Burst)
-}
-
-func FromOther(rate RateLimit) (plugin.Middleware, error) {
-	return NewRateLimit(rate.Requests, rate.Variable, rate.Burst, rate.PeriodSeconds)
-}
-
-// Constructs the middleware from the command line
-func FromCli(c *cli.Context) (plugin.Middleware, error) {
-	return NewRateLimit(c.Int("requests"), c.String("var"), int64(c.Int("burst")), c.Int("period"))
-}
-
-func CliFlags() []cli.Flag {
-	return []cli.Flag{
+	cliFlags := []cli.Flag{
 		cli.StringFlag{Name: "variable, var", Value: "client.ip", Usage: "variable to rate against, e.g. client.ip, request.host or request.header.X-Header"},
 		cli.IntFlag{Name: "requests", Value: 1, Usage: "amount of requests"},
 		cli.IntFlag{Name: "period", Value: 1, Usage: "rate limit period in seconds"},
 		cli.IntFlag{Name: "burst", Value: 1, Usage: "allowed burst"},
+		cli.StringFlag{Name: "configVar", Value: "", Usage: "variable to retrieve rate config from, e.g. request.header.X-Rates-Json"},
 	}
+	return &plugin.MiddlewareSpec{
+		Type:      "ratelimit",
+		FromOther: FromOther,
+		FromCli:   FromCli,
+		CliFlags:  cliFlags,
+	}
+}
+
+// FromOther constructs middleware factory from `RateLimitSpec`.
+func FromOther(config Config) (plugin.Middleware, error) {
+	mapper, err := limit.VariableToMapper(config.Variable)
+	if err != nil {
+		return nil, err
+	}
+
+	configMapper, err := configMapperFromVar(config.ConfigVar)
+	if err != nil {
+		return nil, err
+	}
+
+	period := time.Duration(config.PeriodSeconds) * time.Second
+	defaultRates := tokenbucket.NewRateSet()
+	if err = defaultRates.Add(period, config.Requests, config.Burst); err != nil {
+		return nil, err
+	}
+
+	return &Factory{
+		config:       &config,
+		mapper:       mapper,
+		configMapper: configMapper,
+		defaultRates: defaultRates,
+	}, nil
+}
+
+// FromCli constructs the middleware from the command line arguments.
+func FromCli(c *cli.Context) (plugin.Middleware, error) {
+	return FromOther(
+		Config{
+			PeriodSeconds: int64(c.Int("period")),
+			Requests:      int64(c.Int("requests")),
+			Burst:         int64(c.Int("burst")),
+			Variable:      c.String("var"),
+			ConfigVar:     c.String("configVar")})
+}
+
+// Config defines configuration parameters of `Factory`.
+type Config struct {
+	// Period in seconds, e.g. 3600 to set up hourly rates.
+	PeriodSeconds int64
+	// Burst count, allows some extra variance for requests exceeding the
+	// average rate.
+	Burst int64
+	// Variable defines how the limiting should be done. E.g. 'client.ip' or
+	// 'request.header.X-My-Header'
+	Variable string
+	// Allowed average requests
+	Requests int64
+	// Variable that defines the source of token bucket configurations. This
+	// parameter is optional. If omitted then the rate given via `Requests`,
+	// `Burst`, and `PeriodSeconds` is used.
+	ConfigVar string
+}
+
+// RateLimitFactory implements `middleware.Middleware`. Its `NewMiddleware`
+// method creates a rate limiting middleware based on the token bucket
+// algorithm http://en.wikipedia.org/wiki/Token_bucket.
+type Factory struct {
+	config       *Config
+	mapper       limit.MapperFn
+	configMapper tokenbucket.ConfigMapperFn
+	defaultRates *tokenbucket.RateSet
+	clock        timetools.TimeProvider
+}
+
+// Returns vulcan library compatible middleware
+func (f *Factory) NewMiddleware() (middleware.Middleware, error) {
+	return tokenbucket.NewLimiter(f.defaultRates, 0, f.mapper, f.configMapper, f.clock)
+}
+
+func (f *Factory) String() string {
+	return fmt.Sprintf("reqs/%s=%d, burst=%d, var=%s, configVar=%s",
+		time.Duration(f.config.PeriodSeconds)*time.Second, f.config.Requests,
+		f.config.Burst, f.config.Variable, f.config.ConfigVar)
+}
+
+func configMapperFromVar(variable string) (tokenbucket.ConfigMapperFn, error) {
+	if variable == "" {
+		return nil, nil
+	}
+
+	m, err := limit.MakeTokenMapperFromVariable(variable)
+	if err != nil {
+		return nil, err
+	}
+
+	return func(r request.Request) (*tokenbucket.RateSet, error) {
+		jsonString, err := m(r)
+		if err != nil {
+			return nil, err
+		}
+
+		var specs []rateSpec
+		if err = json.Unmarshal([]byte(jsonString), &specs); err != nil {
+			return nil, err
+		}
+
+		rateSet := tokenbucket.NewRateSet()
+		for _, s := range specs {
+			period := time.Duration(s.PeriodSec) * time.Second
+			if s.Burst == 0 {
+				s.Burst = s.Average
+			}
+			if err = rateSet.Add(period, s.Average, s.Burst); err != nil {
+				return nil, err
+			}
+		}
+		return rateSet, nil
+	}, nil
+}
+
+// rateSpec is used to serialize token bucket rates to JSON. Note that the
+// `burst` parameter can be omitted in the serialized form, in that case it is
+// considered to be equal to `average`.
+type rateSpec struct {
+	PeriodSec int64
+	Average   int64
+	Burst     int64
 }
--- plugin/ratelimit/ratelimit_test.go
+++ plugin/ratelimit/ratelimit_test.go
@@ -1,15 +1,28 @@
 package ratelimit
 
 import (
-	"github.com/mailgun/vulcand/Godeps/_workspace/src/github.com/codegangsta/cli"
-	. "github.com/mailgun/vulcand/Godeps/_workspace/src/gopkg.in/check.v1"
-	"github.com/mailgun/vulcand/plugin"
+	"fmt"
+	"net/http"
 	"testing"
+	"time"
+
+	"github.com/mailgun/vulcand/Godeps/_workspace/src/github.com/codegangsta/cli"
+	"github.com/mailgun/vulcand/Godeps/_workspace/src/github.com/mailgun/timetools"
+	"github.com/mailgun/vulcand/Godeps/_workspace/src/github.com/mailgun/vulcan/request"
+	"github.com/mailgun/vulcand/plugin"
+	. "github.com/mailgun/vulcand/Godeps/_workspace/src/gopkg.in/check.v1"
 )
 
 func TestRL(t *testing.T) { TestingT(t) }
 
 type RateLimitSuite struct {
+	clock *timetools.FreezedTime
+}
+
+func (s *RateLimitSuite) SetUpSuite(c *C) {
+	s.clock = &timetools.FreezedTime{
+		CurrentTime: time.Date(2012, 3, 4, 5, 6, 7, 0, time.UTC),
+	}
 }
 
 var _ = Suite(&RateLimitSuite{})
@@ -20,49 +33,102 @@ func (s *RateLimitSuite) TestSpecIsOK(c *C) {
 	c.Assert(plugin.NewRegistry().AddSpec(GetSpec()), IsNil)
 }
 
-func (s *RateLimitSuite) TestNewRateLimitSuccess(c *C) {
-	rl, err := NewRateLimit(10, "client.ip", 1, 1)
-	c.Assert(rl, NotNil)
+func (s *RateLimitSuite) TestFromOther(c *C) {
+	rlf, err := FromOther(
+		Config{
+			PeriodSeconds: 1,
+			Requests:      1,
+			Burst:         10,
+			Variable:      "client.ip",
+			ConfigVar:     "request.header.X-Rates",
+		})
+	c.Assert(rlf, NotNil)
 	c.Assert(err, IsNil)
+	c.Assert(fmt.Sprint(rlf), Equals, "reqs/1s=1, burst=10, var=client.ip, configVar=request.header.X-Rates")
 
-	c.Assert(rl.String(), Not(Equals), "")
-
-	out, err := rl.NewMiddleware()
+	out, err := rlf.NewMiddleware()
 	c.Assert(out, NotNil)
 	c.Assert(err, IsNil)
 }
 
-func (s *RateLimitSuite) TestNewRateLimitBadParams(c *C) {
+func (s *RateLimitSuite) TestFromOtherNoConfigVar(c *C) {
+	rlf, err := FromOther(
+		Config{
+			PeriodSeconds: 1,
+			Requests:      1,
+			Burst:         10,
+			Variable:      "client.ip",
+			ConfigVar:     "",
+		})
+	c.Assert(rlf, NotNil)
+	c.Assert(err, IsNil)
+
+	out, err := rlf.NewMiddleware()
+	c.Assert(out, NotNil)
+	c.Assert(err, IsNil)
+}
+
+func (s *RateLimitSuite) TestFromOtherBadParams(c *C) {
 	// Unknown variable
-	_, err := NewRateLimit(10, "client ip", 1, 1)
+	_, err := FromOther(
+		Config{
+			PeriodSeconds: 1,
+			Requests:      1,
+			Burst:         10,
+			Variable:      "foo",
+			ConfigVar:     "request.header.X-Rates-Json",
+		})
 	c.Assert(err, NotNil)
 
 	// Negative requests
-	_, err = NewRateLimit(-10, "client.ip", 1, 1)
+	_, err = FromOther(
+		Config{
+			PeriodSeconds: 1,
+			Requests:      -1,
+			Burst:         10,
+			Variable:      "client.ip",
+			ConfigVar:     "request.header.X-Rates-Json",
+		})
 	c.Assert(err, NotNil)
 
 	// Negative burst
-	_, err = NewRateLimit(10, "client.ip", -1, 1)
+	_, err = FromOther(
+		Config{
+			PeriodSeconds: 1,
+			Requests:      1,
+			Burst:         -1,
+			Variable:      "client.ip",
+			ConfigVar:     "request.header.X-Rates-Json",
+		})
 	c.Assert(err, NotNil)
 
 	// Negative period
-	_, err = NewRateLimit(10, "client.ip", 1, -1)
+	_, err = FromOther(
+		Config{
+			PeriodSeconds: -1,
+			Requests:      1,
+			Burst:         10,
+			Variable:      "client.ip",
+			ConfigVar:     "request.header.X-Rates-Json",
+		})
+	c.Assert(err, NotNil)
+
+	// Unknown config variable
+	_, err = FromOther(
+		Config{
+			PeriodSeconds: 1,
+			Requests:      1,
+			Burst:         10,
+			Variable:      "client.ip",
+			ConfigVar:     "foo",
+		})
 	c.Assert(err, NotNil)
 }
 
-func (s *RateLimitSuite) TestNewRateLimitFromOther(c *C) {
-	rl, err := NewRateLimit(10, "client.ip", 1, 1)
-	c.Assert(rl, NotNil)
-	c.Assert(err, IsNil)
-
-	out, err := FromOther(*rl)
-	c.Assert(err, IsNil)
-	c.Assert(out, DeepEquals, rl)
-}
-
-func (s *RateLimitSuite) TestNewRateLimitFromCliOk(c *C) {
+func (s *RateLimitSuite) TestFromCli(c *C) {
 	app := cli.NewApp()
 	app.Name = "test"
+	app.Flags = GetSpec().CliFlags
 	executed := false
 	app.Action = func(ctx *cli.Context) {
 		executed = true
@@ -70,13 +136,212 @@ func (s *RateLimitSuite) TestNewRateLimitFromCliOk(c *C) {
 		c.Assert(out, NotNil)
 		c.Assert(err, IsNil)
 
-		rl := out.(*RateLimit)
-		c.Assert(rl.Variable, Equals, "client.ip")
-		c.Assert(rl.Requests, Equals, 10)
-		c.Assert(rl.Burst, Equals, int64(3))
-		c.Assert(rl.PeriodSeconds, Equals, 4)
+		rlf := out.(*Factory)
+		m, err := rlf.NewMiddleware()
+		c.Assert(m, NotNil)
+		c.Assert(err, IsNil)
 	}
-	app.Flags = CliFlags()
 	app.Run([]string{"test", "--var=client.ip", "--requests=10", "--burst=3", "--period=4"})
 	c.Assert(executed, Equals, true)
+}
+
+// Middleware instance created by the factory is using rates configuration
+// from the respective request header.
+func (s *RateLimitSuite) TestRequestProcessing(c *C) {
+	// Given
+	rlf, _ := FromOther(
+		Config{
+			PeriodSeconds: 1,
+			Requests:      1,
+			Burst:         1,
+			Variable:      "client.ip",
+			ConfigVar:     "request.header.X-Rates",
+		})
+	// Inject deterministic time provider into the middleware instance
+	(rlf.(*Factory)).clock = s.clock
+
+	rli, _ := rlf.NewMiddleware()
+
+	request := &request.BaseRequest{
+		HttpRequest: &http.Request{
+			RemoteAddr: "1.2.3.4",
+			Header: http.Header(map[string][]string{
+				"X-Rates": []string{`[{"PeriodSec": 1, "Average": 2}]`}}),
+		},
+	}
+
+	// When/Then: The configured rate is applied, which 2 request/second, note
+	// that the default rate is 1 request/second.
+	response, err := rli.ProcessRequest(request) // Processed
+	c.Assert(response, IsNil)
+	c.Assert(err, IsNil)
+	response, err = rli.ProcessRequest(request) // Processed
+	c.Assert(response, IsNil)
+	c.Assert(err, IsNil)
+	response, err = rli.ProcessRequest(request) // Rejected
+	c.Assert(response, NotNil)
+	c.Assert(err, IsNil)
+
+	s.clock.Sleep(time.Second)
+	response, err = rli.ProcessRequest(request) // Processed
+	c.Assert(response, IsNil)
+	c.Assert(err, IsNil)
+}
+
+func (s *RateLimitSuite) TestRequestProcessingEmptyConfig(c *C) {
+	// Given
+	rlf, _ := FromOther(
+	Config{
+		PeriodSeconds: 1,
+		Requests:      1,
+		Burst:         1,
+		Variable:      "client.ip",
+		ConfigVar:     "request.header.X-Rates",
+	})
+	// Inject deterministic time provider into the middleware instance
+	(rlf.(*Factory)).clock = s.clock
+
+	rli, _ := rlf.NewMiddleware()
+
+	request := &request.BaseRequest{
+		HttpRequest: &http.Request{
+			RemoteAddr: "1.2.3.4",
+			Header: http.Header(map[string][]string{
+				"X-Rates": []string{`[]`}}),
+		},
+	}
+
+	// When/Then: The default rate of 1 request/second is used.
+	response, err := rli.ProcessRequest(request) // Processed
+	c.Assert(response, IsNil)
+	c.Assert(err, IsNil)
+	response, err = rli.ProcessRequest(request) // Rejected
+	c.Assert(response, NotNil)
+	c.Assert(err, IsNil)
+
+	s.clock.Sleep(time.Second)
+	response, err = rli.ProcessRequest(request) // Processed
+	c.Assert(response, IsNil)
+	c.Assert(err, IsNil)
+}
+
+func (s *RateLimitSuite) TestRequestProcessingNoHeader(c *C) {
+	// Given
+	rlf, _ := FromOther(
+	Config{
+		PeriodSeconds: 1,
+		Requests:      1,
+		Burst:         1,
+		Variable:      "client.ip",
+		ConfigVar:     "request.header.X-Rates",
+	})
+	// Inject deterministic time provider into the middleware instance
+	(rlf.(*Factory)).clock = s.clock
+
+	rli, _ := rlf.NewMiddleware()
+
+	request := &request.BaseRequest{
+		HttpRequest: &http.Request{
+			RemoteAddr: "1.2.3.4",
+		},
+	}
+
+	// When/Then: The default rate of 1 request/second is used.
+	response, err := rli.ProcessRequest(request) // Processed
+	c.Assert(response, IsNil)
+	c.Assert(err, IsNil)
+	response, err = rli.ProcessRequest(request) // Rejected
+	c.Assert(response, NotNil)
+	c.Assert(err, IsNil)
+
+	s.clock.Sleep(time.Second)
+	response, err = rli.ProcessRequest(request) // Processed
+	c.Assert(response, IsNil)
+	c.Assert(err, IsNil)
+}
+
+// If the rate set from the HTTP header has more then one rate for the same
+// time period defined, then the one mentioned in the list last is used.
+func (s *RateLimitSuite) TestRequestInvalidConfig(c *C) {
+	// Given
+	rlf, _ := FromOther(
+	Config{
+		PeriodSeconds: 1,
+		Requests:      1,
+		Burst:         1,
+		Variable:      "client.ip",
+		ConfigVar:     "request.header.X-Rates",
+	})
+	// Inject deterministic time provider into the middleware instance
+	(rlf.(*Factory)).clock = s.clock
+
+	rli, _ := rlf.NewMiddleware()
+
+	request := &request.BaseRequest{
+		HttpRequest: &http.Request{
+			RemoteAddr: "1.2.3.4",
+			Header: http.Header(map[string][]string{
+				"X-Rates": []string{`[{"PeriodSec": -1, "Average": 10}]`}}),
+		},
+	}
+
+	// When/Then: The default rate of 1 request/second is used.
+	response, err := rli.ProcessRequest(request) // Processed
+	c.Assert(response, IsNil)
+	c.Assert(err, IsNil)
+	response, err = rli.ProcessRequest(request) // Rejected
+	c.Assert(response, NotNil)
+	c.Assert(err, IsNil)
+
+	s.clock.Sleep(time.Second)
+	response, err = rli.ProcessRequest(request) // Processed
+	c.Assert(response, IsNil)
+	c.Assert(err, IsNil)
+}
+
+// If the rate set from the HTTP header has more then one rate for the same
+// time period defined, then the one mentioned in the list last is used.
+func (s *RateLimitSuite) TestRequestProcessingAmbiguousConfig(c *C) {
+	// Given
+	rlf, _ := FromOther(
+	Config{
+		PeriodSeconds: 1,
+		Requests:      1,
+		Burst:         1,
+		Variable:      "client.ip",
+		ConfigVar:     "request.header.X-Rates",
+	})
+	// Inject deterministic time provider into the middleware instance
+	(rlf.(*Factory)).clock = s.clock
+
+	rli, _ := rlf.NewMiddleware()
+
+	request := &request.BaseRequest{
+		HttpRequest: &http.Request{
+			RemoteAddr: "1.2.3.4",
+			Header: http.Header(map[string][]string{
+				"X-Rates": []string{`[{"PeriodSec": 1, "Average": 10},
+					                  {"PeriodSec": 1, "Average": 3}]`}}),
+		},
+	}
+
+	// When/Then: The last of configured rates with the same period is applied,
+    // which 3 request/second, note that the default rate is 1 request/second.
+	response, err := rli.ProcessRequest(request) // Processed
+	c.Assert(response, IsNil)
+	c.Assert(err, IsNil)
+	response, err = rli.ProcessRequest(request) // Processed
+	c.Assert(response, IsNil)
+	c.Assert(err, IsNil)
+	response, err = rli.ProcessRequest(request) // Processed
+	c.Assert(response, IsNil)
+	c.Assert(err, IsNil)
+	response, err = rli.ProcessRequest(request) // Rejected
+	c.Assert(response, NotNil)
+	c.Assert(err, IsNil)
+
+	s.clock.Sleep(time.Second)
+	response, err = rli.ProcessRequest(request) // Processed
+	c.Assert(response, IsNil)
+	c.Assert(err, IsNil)
 }
